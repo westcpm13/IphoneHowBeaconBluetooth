690b23102b35f820626194a465348bae169c1195
Pawel Trojan <paweltrojan@MacBook-Pro-Pawel.local>
2017-07-10 03:03:16 +0200

    delegate set up beacon

--- IphoneHowBeaconBluetooth/ViewController.swift
+++ IphoneHowBeaconBluetooth/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
 class ViewController: UIViewController {
 
+    private var region: CLBeaconRegion?
+    private final let sampleUUID = "B2DCB64-3300-4G62-8A11-F6E7A06E4BC0"
+    private var peripheralManager: CBPeripheralManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.region = CLBeaconRegion(proximityUUID: UUID(uuidString: self.sampleUUID)!, identifier: "com.ptrojan.IphoneHowBeaconBluetooth")
+        self.peripheralManager = CBPeripheralManager(delegate: self, queue: DispatchQueue.main)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: CBPeripheralManagerDelegate {
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        
+        var statusMessage = ""
+        
+        switch peripheral.state {
+        case .poweredOn:
+            statusMessage = "Bluetooth Status: Turned On"
+            
+        case .poweredOff:
+            statusMessage = "Bluetooth Status: Turned Off"
+            
+        case .resetting:
+            statusMessage = "Bluetooth Status: Resetting"
+            
+        case .unauthorized:
+            statusMessage = "Bluetooth Status: Not Authorized"
+            
+        case .unsupported:
+            statusMessage = "Bluetooth Status: Not Supported"
+            
+        default:
+            statusMessage = "Bluetooth Status: Unknown"
+        }
+        
+        print(statusMessage)
+        
+    }
+
+}
